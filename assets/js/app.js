35c61775ff8d228f71a49906dbe13ccd4056d27b
Kevin <quachvanbach@gmail.com>
2024-03-05 20:13:18 +0700

    update 05/03/2024

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,38 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const btn_steps = $$('.btn-step-item');
+const step_items = $$('.step-content__item')
+
+btn_steps.forEach((btn_step, index) => {
+    const step_item = step_items[index];
+    btn_step.addEventListener('click', function () {
+        $('.btn-step-item.active').classList.remove('active');
+        $('.step-content__item.active').classList.remove('active');
+
+
+        this.classList.add('active');
+        step_item.classList.add('active')
+    })
+})
+
+const btn_next_step = $('.btn-next-step');
+btn_next_step.addEventListener('click', function () {
+    var activeIndex = 0
+    step_items.forEach((step_item, index) => {
+        if (step_item.classList.contains('active')) {
+            step_items.item(index).remove('active');
+            activeIndex = index + 1;
+        }
+    })
+    if (activeIndex < step_items.length) {
+        console.log("active index: " + activeIndex)
+
+    } else {
+        activeIndex = 0;
+    }
+    step_items.item(activeIndex).classList.add('active');
+});
 const signup_btn = $('.btn-sign-up');
 const signin_btn = $('.btn-sign-in');
 const account_modal = $('.modal.account-modal')
--- vendor/tailwindcss/tailwind.config.js
+++ vendor/tailwindcss/tailwind.config.js
@@ -2,15 +2,26 @@
 module.exports = {
     content: ["../../**/*.{html,js}"],
     theme: {
+        plugins: [],
         extend: {
             backgroundImage: {
                 'content-bg': 'url("../../assets/images/content-background.svg")',
+                'tools-button-icon': 'url("../../assets/images/tools-button-icon.svg")',
             },
             flex: {
                 '50': '0 0 40%'
             }
         },
-        plugins: [],
+        screens: {
+            // 'xs': {'max': '640px'},
+            // 'sm': {'min': '641px', 'max': '767px'},
+            // 'md': {'min': '768px', 'max': '1023px'},
+            // 'lg': {'min': '1024px', 'max': '1279px'},
+            // 'xl': {'min': '1280px', 'max': '1535px'},
+            // '2xl': {'min': '1536px'},
+            'sm': {'max': '750px'},
+            'lg': {'min': '751px'}
+        },
     }
 }
 
